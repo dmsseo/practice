39e892832d028670d95c91a69564f92066b5504f
dmsseo <lindajang1614@gmail.com>
2025-08-22 20:39:51 +0900

    Add files via upload

---
+++ practice/C/C-Study/1003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int age = 20;
+	float tall = 123.4;
+	char blood_type = 'A'; 
+	
+	printf("나이: %d\n", age);
+	printf("키: %.1lfcm\n", tall);
+	printf("혈액형: %c", blood_type);
+	return 0;  
+} 
